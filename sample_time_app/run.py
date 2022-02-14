9b58122ede761e80e4e344841d6c2bad9c5a9f80
wz2392 <99241150+wz2392@users.noreply.github.com>
2022-02-14 03:44:42 -0500

    Add files via upload

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from datetime import datetime
+import pytz
+
 app = Flask(__name__)
 
 
@@ -6,6 +9,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def return_time():
+    now = datetime.now(pytz.timezone('America/New_York'))
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 app.run(host='0.0.0.0',
         port=8080,
